6b885d8a5fc78608fa349e87600d405dfb345d6f
prestcmin <prestoncminnick@gmail.com>
2026-04-25 15:23:32 -0600

    completed steps for prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string userInput = Console.ReadLine();
+        int grade_percent = int.Parse(userInput);
+
+        string letter = "";
+
+        if (grade_percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade_percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade_percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade_percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (grade_percent >= 70)
+        {
+            Console.WriteLine("You have passed the course! Congrats!");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the course. Better luck next time!");
+        }
     }
 }
