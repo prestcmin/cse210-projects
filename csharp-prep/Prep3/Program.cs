07f766194e3caaac6a87d1703744d8dc6f804e69
prestcmin <prestoncminnick@gmail.com>
2026-04-30 12:39:37 -0600

    completed steps for prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1,11);
+        int guess = 0;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();    
+            guess = int.Parse(input);
+            
+            if (guess > num)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else if (guess < num)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (num != guess);
+        
     }
 }
