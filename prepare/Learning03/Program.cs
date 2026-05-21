55b899768c4689b8f50b3b89f058d40c5b318876
prestcmin <prestoncminnick@gmail.com>
2026-05-21 07:33:11 -0600

    completed steps for learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+
+        Fraction fraction5 = new Fraction();
+        Random randomnum = new Random();
+        for (int i = 0; i < 20; i++)
+        {
+            int topValue = randomnum.Next(1, 11);
+            int bottomValue = randomnum.Next(1, 11);
+            fraction5.SetTop(topValue);
+            fraction5.SetBottom(bottomValue);
+            Console.Write($"Fraction {i + 1}: ");
+            Console.Write($"String: {fraction5.GetFractionString()} ");
+            Console.WriteLine($"Number: {fraction5.GetDecimalValue()}");
+        }
     }
 }
